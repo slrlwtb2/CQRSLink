ac00fe318c7a46d1d333e74bb579a80ebb0cc191
EFINANCETHAI\thanasiri.p <thanasiri@efinancethai.com>
2025-04-08 21:16:34 +0700

    Added method overload for easy send command and query

--- CQRSLink/Interfaces/ICQRSLink.cs
+++ CQRSLink/Interfaces/ICQRSLink.cs
@@ -2,10 +2,15 @@
 {
     public interface ICQRSLink
     {
+        // Command
         Task<TResult> SendCommand<TCommand, TResult>(TCommand command, CancellationToken cancellationToken = default)
             where TCommand : ICommand<TResult>;
+        Task<TResult> SendCommand<TResult>(ICommand<TResult> command, CancellationToken cancellationToken = default);
 
+
+        // Query
         Task<TResult> SendQuery<TQuery, TResult>(TQuery query, CancellationToken cancellationToken = default)
             where TQuery : IQuery<TResult>;
+        Task<TResult> SendQuery<TResult>(IQuery<TResult> query, CancellationToken cancellationToken = default);
     }
 }
